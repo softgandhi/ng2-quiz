6d6c5e3893c529ab178c8d3ecc84809164be9cb1
Anurag Gandhi <angand@microsoft.com>
2018-06-19 20:24:08 +0530

    timer fix

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -70,7 +70,7 @@ export class QuizComponent implements OnInit {
   }
 
   parseTime(totalSeconds: number) {
-    let mins: string | number = Math.round(totalSeconds / 60);
+    let mins: string | number = Math.floor(totalSeconds / 60);
     let secs: string | number = Math.round(totalSeconds % 60);
     mins = (mins < 10 ? '0' : '') + mins;
     secs = (secs < 10 ? '0' : '') + secs;
